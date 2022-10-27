e3f241fc652065f8360f5de36eb79a35aef3d721
Kyang0524 <xiaoyang7481@gmail.com>
2022-10-27 20:26:28 +0800

    add room

--- LoginAndRegister.php
+++ LoginAndRegister.php
@@ -56,8 +56,8 @@ if (isset($_POST['reg_user'])) {
   	$query = "INSERT INTO users (UID, username, email, password, money) 
   			  VALUES('$UID', '$username', '$email', '$password', '$money')";
   	mysqli_query($db, $query);
-  	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "You are now logged in";
+  	//$_SESSION['username'] = $username;
+  	//$_SESSION['success'] = "You are now logged in";
   	header('location: home.html');
   }
 }
@@ -79,8 +79,8 @@ if (isset($_POST['login_user'])) {
   	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
   	$results = mysqli_query($db, $query);
   	if (mysqli_num_rows($results) == 1) {
-  	  $_SESSION['username'] = $username;
-  	  $_SESSION['success'] = "You are now logged in";
+  	  //$_SESSION['username'] = $username;
+  	  //$_SESSION['success'] = "You are now logged in";
   	  header('location: home.html');
   	}else {
   		array_push($errors, "Wrong username/password combination");
